d36dd1d0751644bea3051f0c92fc6651ee283c64
Dhi Aurrahman <diorahman@rockybars.com>
2014-07-21 15:41:08 +0700

    Changes: initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var request = require('request');
+
+var app = express();
+app.get('/c1', function(req, res){
+  var url = req.query.url;
+  request(url).pipe(res);
+});
+
+app.listen(3000);
